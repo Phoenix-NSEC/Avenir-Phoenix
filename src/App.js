4917b460867e6b7d790010f6d7daedb8c0f9a2e8
Kunal-deve1oper <ghoshkunal106@gmail.com>
2023-03-31 20:23:21 +0530

    structure added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home';
+import Events from './pages/Events/Events';
+import Aboutus from './pages/About/Aboutus';
+import Contact from './pages/Contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      Welcome to Avenir
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/events' element={<Events/>}/>
+        <Route path='/about' element={<Aboutus/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/About/Aboutus.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Aboutus() {
+  return (
+    <div>
+      About us page
+    </div>
+  )
+}
+
+export default Aboutus
