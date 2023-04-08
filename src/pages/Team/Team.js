65047e4de095dd39e62e13224a25499e28566dec
laughing-nerd <riturajpaul99@gmail.com>
2023-04-08 22:25:02 +0530

    New Teams UI. Needs some fix

--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Team.css';
 import '../../Assets/Css/navbarGlitch.css';
 import Card from '../../components/Card';
 import team from '../../Data/TeamData';
 import Socials from '../../components/Socials';
+import './glitch-teams.css';
 
 function nCard(val) {
   return (<Card
@@ -16,25 +17,79 @@ function nCard(val) {
     linkedin={val.linkedin}
   />)
 }
-
 function Team() {
+
+  const [core, showCore] = useState(true);
+  const [working, showWorking] = useState(true);
+  const [wing, showWing] = useState(true);
+
   return (
     <>
-        <div className="card-container-teams-core h-screen flex justify-items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5">
+      <div className='main-wrapper-teams'>
+        <div className="buttons-teams">
+          <button className={core && working && wing ? 'glitchex glitchex-teams' : 'cybr-btn-teams'} onClick={() => { 
+            showCore(true);
+            showWorking(true);
+            showWing(true);
+            }} aria-hidden>All
+            <span class="cybr-btn-teams__glitch"></span>
+            <span class="cybr-btn-teams__tag"></span>
+            </button>
+          <button className={core && !working && !wing ? 'glitchex glitchex-teams' : 'cybr-btn-teams'} onClick={() => { 
+            showCore(true);
+            showWorking(false);
+            showWing(false);
+            }}>Core Team
+            <span class="cybr-btn-teams__glitch"></span>
+            <span class="cybr-btn-teams__tag"></span>
+            </button>
+          <button className={!core && working && !wing ? 'glitchex glitchex-teams' : 'cybr-btn-teams'} onClick={() => { 
+            showCore(false);
+            showWorking(true);
+            showWing(false);
+            }}>Working Team
+            <span class="cybr-btn-teams__glitch"></span>
+            <span class="cybr-btn-teams__tag"></span>
+            </button>
+          <button className={!core && !working && wing ? 'glitchex glitchex-teams' : 'cybr-btn-teams'} onClick={() => { 
+            showCore(false);
+            showWorking(false);
+            showWing(true);
+            }}>Wing Coordinators
+            <span class="cybr-btn-teams__glitch"></span>
+            <span class="cybr-btn-teams__tag"></span>
+            </button>
+        </div>
+
+
+        {core ? <div className="card-container-teams-core h-screen flex justify-items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5">
           {team.core.map(nCard)}
           {/* This is Core Div */}
-        </div>
-
-        <div className="card-container-teams-working h-screen flex justify-items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5">
+        </div> : null }
+        {working ? <div className="card-container-teams-working h-screen flex justify-items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5">
           {team.working.map(nCard)}
           {/* This is Working div */}
-        </div>
-
-        <div className="card-container-teams-wing h-screen flex justify-items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5">
+        </div> : null}
+        {wing ? <div className="card-container-teams-wing h-screen flex justify-items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5">
           {team.wing.map(nCard)}
           {/* This is wing div */}
-        </div>
+        </div> : null}
 
+
+        {/* <div className="card-container-teams-core h-screen flex justify-items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5">
+          {team.core.map(nCard)} */}
+        {/* This is Core Div */}
+        {/* </div> */}
+        {/* <div className="card-container-teams-working h-screen flex justify-items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5">
+          {team.working.map(nCard)}
+          This is Working div
+        </div> */}
+        {/* <div className="card-container-teams-wing h-screen flex justify-items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5">
+          {team.wing.map(nCard)}
+          This is wing div
+        </div> */}
+
+      </div>
       <div className="only-for-desktop">
         <Socials />
       </div>
@@ -42,4 +97,4 @@ function Team() {
   )
 }
 
-export default Team
+export default Team;
