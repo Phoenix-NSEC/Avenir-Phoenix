2157c7ffd500a05d5843f0a17d13e9ae7bf39a91
Abhinil-07 <abhinilnath10@gmail.com>
2023-04-17 00:13:24 +0530

    Updated About Us page

--- src/pages/About/Aboutus.js
+++ src/pages/About/Aboutus.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useState } from "react";
 import "./Style.css";
 import aboutAvenir from "../../Assets/Images/logo3.png";
+import aboutNSEC from "../../Assets/Images/NSEC.jpg"
+import aboutPhoenix from "../../Assets/Images/logo1.png"
 import { BsFacebook, BsInstagram, BsLinkedin, BsYoutube } from "react-icons/bs";
 import { FiMail } from "react-icons/fi";
 import "../Events/glitch.css";
@@ -17,42 +19,46 @@ function Aboutus() {
           ABOUT NSEC
         </p>
         <div className="container mx-auto flex-col text-center justify-center items-center font-chakra">
-          <div className="mt-4 mx-2 flex flex-col md:flex-row text-center justify-center items-center">
+          <div className="mt-4 mx-2 flex flex-col md:flex-row text-center justify-center items-center md:pr-[20px] md:pl-[16px] ">
             <div className=" text-white text-center md:text-right font-chakra text-[16px] md:text-[21px]">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
-              voluptatem quae? Iste aperiam ullam esse, porro corporis
-              perspiciatis magnam asperiores quas quaerat molestiae assumenda
-              sequi excepturi rem enim maiores quos provident vel?Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Obcaecati eius porro
-              dolor veritatis possimus pariatur non facilis magni exercitationem
-              delectus.
+            Netaji Subhash Engineering College or NSEC is a graduate and undergraduate engineering college of eastern India located in Kolkata, West Bengal, India. Established in 1998, it is situated at Techno City, Panchpota, Garia in the southern part of Kolkata.
             </div>
 
             <img
-              src="https://www.collegesignal.com/images/colleges/cz_5922c05980d57172229/cover/nsec_cover-m4032-original.jpg"
+              src={aboutNSEC}
               alt=""
-              className="m-4 w-[400px] h-[200px]"
+              className="m-4 h-[250px] w-[280px] md:h-[300px] md:w-[400px]"
             />
+          </div>
+          <p className="py-[40px] mx-1 font-bold text-center justify-center text-white text-4xl md:text-5xl font-orbitron">
+            {" "}
+            ABOUT PHOENIX
+          </p>
+          <div className="mt-4 mx-2 mb-4 flex flex-col-reverse md:flex-row text-center justify-center items-center md:pr-[25px] md:pl-[20px] ">
+            <img
+              src={aboutPhoenix}
+              alt=""
+              className="m-4 h-[250px] w-[200px] md:h-[300px] md:w-[350px]"
+            />
+
+            <div className="text-center md:text-left text-white  font-chakra text-[16px] md:text-[21px]">
+            PHOENIX is the official tech club of Netaji Subhash Engineering College. A club which looks into the overall development of the students of this college. It was founded by a group of like-minded people in January 2006 with the aim of inculcating values of friendship, teamwork and leadership as well as increases the technical skills like coding and robotics of a student. PHOENIX plays a crucial role in bridging the gap between the college and corporate life. A club which provides each individual a stage to shine.
+            </div>
           </div>
           <p className="py-[40px] mx-1 font-bold text-center justify-center text-white text-4xl md:text-5xl font-orbitron">
             {" "}
             ABOUT AVENIR
           </p>
-          <div className="mt-4 mx-2 mb-4 flex flex-col-reverse md:flex-row text-center justify-center items-center ">
+          <div className="mt-4 mx-2 flex flex-col md:flex-row text-center justify-center items-center md:pr-[25px] md:pl-[20px]">
+            <div className=" text-white text-center md:text-right font-chakra text-[16px] md:text-[21px]">
+            Avenir - The Annual Techno-Management Fest of NSEC, stands amongst the top technical fests in the city with enthusiastic participation from students of premier institutes in and around the state. The three day spectacular event yields a footfall of more than 7000+ students , thus making it a perfect platform to explore the realm of technical innovation and to witness the best minds striving for excellence. The idea of Avenir unravels the big aspirations hidden inside a creative mind and promises the ultimate platform to showcase talent.
+            </div>
+
             <img
               src={aboutAvenir}
               alt=""
               className="m-4 h-[200px] w-[200px] md:h-[300px] md:w-[350px]"
             />
-
-            <div className="text-center md:text-left text-white  font-chakra text-[16px] md:text-[21px]">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci
-              rem itaque officia excepturi id expedita maxime vero enim suscipit
-              non! Qui adipisci maiores facere fugit incidunt possimus non at
-              commodi iure quos beatae officia officiis laborum voluptatibus
-              vitae dolores voluptatem cumque error atque, dignissimos porro
-              animi! Dolores eius blanditiis veniam.
-            </div>
           </div>
         </div>
         <p className=" text-center justify-center text-white text-4xl md:text-5xl font-bold font-orbitron py-4">
@@ -64,7 +70,7 @@ function Aboutus() {
             <div className="box1 md:w-67 w-[10px] ">
               <span class="borderLine"></span>
               <form
-                action="https://formspree.io/f/xrgvyapq"
+                action="https://formspree.io/f/xpzeokzr"
                 method="POST"
                 className="flex flex-col space-y-4"
                 // onSubmit={handleSubmitForm}
@@ -93,8 +99,7 @@ function Aboutus() {
                   <span>Email Address</span>
                   <i></i>
                 </div>
-                <div className="inputbox "
-                style ={{marginBottom:"20px"}}>
+                <div className="inputbox " style={{ marginBottom: "20px" }}>
                   <input
                     // style={{ background: "none" }}
                     // placeholder="Enter Message"
@@ -116,42 +121,39 @@ function Aboutus() {
                   Send Message
                 </button> */}
 
-                <button
-                  className="cybr-btn w-[300px] mt-4"
-                  
-                >
+                <button className="cybr-btn w-[300px] mt-4">
                   Submit<span aria-hidden>_</span>
                   <span class="cybr-btn__glitch">Send Message</span>
                   <span class="cybr-btn__tag"></span>
                 </button>
               </form>
             </div>
-            <div className="mx-auto mt-[30px] flex flex-col text-[15px] md:flex-row min-w-full md:w-[700px] md:text-[20px]  md:justify-around md:mt-[40px] md:mb-[40px] ">
-              <div className="mx-auto flex-col    ">
+            <div className="mx-auto mt-[30px] flex flex-col text-[15px] md:flex-row min-w-full md:w-[700px] md:text-[20px]  md:justify-around md:mt-[40px] md:mb-[40px] text-center">
+              <div className="mx-auto flex-col">
                 <p className="text-white font-chakra ">
-                  Abhinil Nath :<a href="tel:8910903081"> 8910903081</a>
+                RAAJBIR BAJPAIE :<a href="tel:8240349675"> 8240349675</a>
                 </p>
 
                 <p className="text-white font-chakra py-[2px]">
-                  Abhinil Nath :<a href="tel:8910903081"> 8910903081</a>
+                SANKHADEEP ROY :<a href="tel:7278122176"> 7278122176</a>
                 </p>
 
                 <p className="text-white font-chakra py-[2px]">
-                  Abhinil Nath :<a href="tel:8910903081"> 8910903081</a>
+                RUDRANEEL BHATTACHARYA:<a href="tel:9073560833"> 9073560833</a>
                 </p>
               </div>
 
               <div className="mx-auto flex-col ">
                 <p className="text-white font-chakra ">
-                  Abhinil Nath :<a href="tel:8910903081"> 8910903081</a>
+                SHREYA BAG :<a href="tel:7980960568"> 7980960568</a>
                 </p>
 
                 <p className="text-white font-chakra py-[2px]">
-                  Abhinil Nath :<a href="tel:8910903081"> 8910903081</a>
+                YASH SHAW :<a href="tel:7439558508"> 7439558508</a>
                 </p>
 
                 <p className="text-white font-chakra py-[2px]">
-                  Abhinil Nath :<a href="tel:8910903081"> 8910903081</a>
+                SHARANDEEP MUKHOPADHYAY :<a href="tel:7076397788"> 7076397788</a>
                 </p>
               </div>
             </div>
@@ -201,7 +203,8 @@ function Aboutus() {
                 </a>
 
                 <a
-                  target="https://www.youtube.com/channel/UCBy1iIhw34E7YlHQ8tc4rDA"
+                  target="_blank"
+                  href="https://www.youtube.com/channel/UCBy1iIhw34E7YlHQ8tc4rDA"
                   className="hover:text-slate-300"
                 >
                   <BsYoutube size={"30px"} />
